1357fad3dd948420485f1d04b5f90445cf6525cc
julfiker755 <julfiker755.bd@gmail.com>
2025-04-12 22:01:32 +0600

    doctor list added

--- src/app/(tabs)/doctors.tsx
+++ src/app/(tabs)/doctors.tsx
@@ -1,10 +1,6 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import DoctorsList from "../doctor-list";
 
 export default function Doctors() {
-  return (
-    <View>
-      <Text>Doctors</Text>
-    </View>
-  )
+  return <DoctorsList />;
 }
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,4 @@
-import { StyleSheet } from "react-native";
 import { View } from "@/src/components/Themed";
-import { colors } from "@/src/constants/Colors";
 import Header from "@/src/components/common/header";
 import Categories from "@/src/components/common/categories";
 import FavouriteDoctor from "@/src/components/common/favourite-doctor";
@@ -14,10 +12,3 @@ export default function Home() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    height: "25%",
-    backgroundColor: colors.primary,
-  },
-});
